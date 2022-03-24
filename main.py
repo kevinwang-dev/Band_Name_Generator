90dab42bdfa28ff714c08276cc40322ae1f7fc48
Jiang (Kevin) Wang <66880777+kevinwang-dev@users.noreply.github.com>
2022-03-23 20:44:26 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# Band Name Generator
+print("Welcome to the Band Name Generator App!")
+# prompt user for their city
+city = input("\nWhat is the name of your city?  ")
+# prompt user for their pet name
+pet_name = input("\nWhat is your pet name? ")
+# prompt user for their last name
+last_name = input("\nWhat is your last name? ")
+# print the band name with city name + pet name + lastname
+print("\nThe name of your band could be " + city + " " + pet_name + " " + last_name + "!")
